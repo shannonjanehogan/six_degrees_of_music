9bca3ff7970f17d91fc828decfbd3b58607ecc0f
shannonjanehogan <shannon.jane.hogan@gmail.com>
2018-10-21 15:11:52 -0700

    Add sample API call

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import qs from 'qs';
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+  }
+
+  handleSearchClick(e) {
+    e.preventDefault();
+    return fetch('http://localhost:8080/api/search/new', {
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+      body: qs.stringify({
+        artistone: "Taylor Swift",
+        artisttwo: "Kanye West",
+      }),
+    })
+    .then(response => response.json())
+    .then((responseJson) => {
+      console.log(responseJson)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   render() {
     return (
       <div className="Home">
@@ -14,7 +41,7 @@ class Home extends Component {
         <i class="down arrow icon"></i>
         <SearchBar className="Second-search-bar"/>
         <Link to="/search-results">
-          <button class="ui right labeled icon button">
+          <button onClick={this.handleSearchClick} class="ui right labeled icon button">
             Go!
             <i class="right arrow icon"></i>
           </button>
