586e90293ec86022c85cc4ad46ecd68150a8956d
mariaxtina <mchristinahess@gmail.com>
2018-12-01 18:01:12 -0800

    make other components functional

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input } from 'semantic-ui-react';
 
-class SearchBar extends Component {
-  render() {
-    return (
-      <div className="ui search">
-        <div className="ui icon input">
-          <input className="prompt" type="text" placeholder="Name of Musician"/>
-          <i className="search icon"></i>
-        </div>
-        <div className="results"></div>
-      </div>
-    );
-  }
-}
+const SearchBar = () => (
+  <div className="ui search">
+    <div className="ui icon input">
+      <input className="prompt" type="text" placeholder="Name of Musician"/>
+      <i className="search icon"></i>
+    </div>
+    <div className="results"></div>
+  </div>
+)
 
 export default SearchBar;
