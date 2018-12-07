04063b31cfc1767f190a1a46ee927c321b9a41f1
Dbajj <daniel.bajj@gmail.com>
2018-12-06 20:08:58 -0800

    Implement async queue to display autocomplete results in proper order

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,38 +1,61 @@
 import React, { Component } from 'react'
 import { Search, SearchProps } from 'semantic-ui-react'
 import _ from 'lodash'
+import async from 'async'
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      isLoading: false,
+      results: [], 
+      value: '',
+      valueMap: {},
+    }
+
+    this.searchQueue = async.queue(this.getSearchResults,  1)
+  }
 
   componentWillMount() {
     this.resetComponent();
   }
 
-  resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
+  getSearchResults = (value, callback) => {
+    console.log(value);
+    let url = new URL(`http://104.248.220.9/artists/search/${this.state.value}`)
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      }
+    })
+      .then(response => response.json())
+      .then((responseJson) => {
+        let newResults = _.map(responseJson, (cur) => {
+          return { title: cur.name }
+        })
+
+        this.setState({ isLoading: false, results: newResults, valueMap: { ...this.state.valueMap, [value]: newResults } });
+        callback();
+      }).catch((error) => {
+        console.log(error)
+        callback();
+      })
+  }
+
+  resetComponent = () => this.setState({ isLoading: false, results: [], value: ''})
 
   handleResultSelect = (e, { result }) => this.setState({ value: result.title })
 
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value }, () => {
+      if (this.state.valueMap[value]) return this.setState({ isLoading: false, results: this.state.valueMap[value]})
       if (this.state.value.length < 1) return this.resetComponent()
 
-      let url = new URL(`http://104.248.220.9/artists/search/${this.state.value}`)
-      return fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        }
-      })
-        .then(response => response.json())
-        .then((responseJson) => {
-          let newResults = _.map(responseJson, (cur) => {
-            return {title: cur.name}
-          })
+      this.searchQueue.push(value);
 
-          this.setState({ isLoading: false, results: newResults});
-        }).catch((error) => {
-          console.log(error)
-        })
+      /*
+      */
     })
   }
 
@@ -43,7 +66,7 @@ export default class SearchBar extends Component {
         <Search
           loading={isLoading}
           onResultSelect={this.handleResultSelect}
-          onSearchChange={this.handleSearchChange}
+          onSearchChange={_.debounce(this.handleSearchChange, 500, {leading: true})}
           results={results}
           value={value}
         />
