f173bf3041c401a3dd8523dbdae3140b6fc8e25c
shannonjanehogan <shannon.jane.hogan@gmail.com>
2018-12-13 21:35:59 -0800

    Only show arrow icon if not last card

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Home from './Home';
-import MusicianCard from './MusicianCard';
+import MusicianGroup from './MusicianGroup';
 import { connect } from 'react-redux';
 import { fetchArtistInfo } from './actions/PathAction';
 
@@ -76,30 +76,16 @@ class SearchResults extends Component {
       <div className="SearchResults">
         <Home/>
         <p className="App-intro"> Found 47 paths with 4 degrees of separation from Taylor Swift to Kanye: </p>
-        <div className="ui cards">
           {musicianCardsData.map((cardData, index) =>
-              <MusicianCard
+              <MusicianGroup
                 artistone={cardData.artistone}
                 feature={cardData.feature}
                 artisttwo={cardData.artisttwo}
                 song={cardData.song}
                 key={index}
+                isLastCard={musicianCardsData.length - 1 === index}
               />
           )}
-          <MusicianCard
-            artistone="Taylor Swift"
-            feature="Featuring "
-            artisttwo="Bryan Adams"
-            song="Teardrops on My Guitar, 2004"
-          />
-          <i className="down arrow icon"></i>
-          <MusicianCard
-            artistone="Bryan Adams"
-            feature="Featured by "
-            artisttwo="Tim McGraw"
-            song="Fuck High School Betches, 2006"
-          />
-        </div>
       </div>
     );
   }
