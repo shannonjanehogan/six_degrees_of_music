0baeeedf3a38ae0a447f5c8bf9e2e7d81ac1d84f
shannonjanehogan <shannon.jane.hogan@gmail.com>
2018-12-13 19:28:42 -0800

    Refactor to use artists API

--- src/actions/PathAction.js
+++ src/actions/PathAction.js
@@ -1,5 +1,5 @@
 import { push } from 'react-router-redux';
-// import ArtistsApi from '../api/artists';
+import ArtistsApi from '../api/artists';
 
 export const fetchPathsSuccess = (paths) => dispatch => {
  dispatch({
@@ -10,42 +10,14 @@ export const fetchPathsSuccess = (paths) => dispatch => {
 
 export const fetchPaths = (dispatch) => {
   return async (dispatch) => {
-    let url = new URL("http://104.248.220.9/path")
-
-    const fetchParams = {
-      method: 'GET',
-      headers: {
-       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      }
-    }
-
-    const artistIds = { artist_id_one: 38661, artist_id_two: 69866 }
-    Object.keys(artistIds).forEach(key => url.searchParams.append(key, artistIds[key]))
-
     try {
-      const response = await fetch(url, fetchParams)
+      const response = await ArtistsApi.findPath(1, 2)
       const responseJson = await response.json()
-
       // need to get the push working
       dispatch(push('/search-results'))
-      
       dispatch(fetchPathsSuccess(responseJson))
     } catch (error) {
       console.log('An error occurred.', error)
     }
-
-    // what is this?
-
-    // ArtistsApi
-    //   .findPath(1, 2)
-    //   .then((jsonResponse) => {
-    //     return jsonResponse.json();
-    //   }).then((response) => {
-    //     console.log(response)
-    //     dispatch(push('/search-results'));
-    //     dispatch(fetchPathsSuccess(response));
-    //   }).catch((error) => {
-    //     // TODO
-    //   });
   };
 }
--- src/api/artists.js
+++ src/api/artists.js
@@ -4,7 +4,6 @@ class ArtistsApi {
     // The fetch library does not support search params on GET requests, so this
     // is the workaround recommended by the library maintainers:
     // See: https://github.com/github/fetch/issues/256
-    console.log("IN FIND PATH");
     let url = new URL("http://104.248.220.9/path"),
     params = { artist_id_one: 38661, artist_id_two: 69866 }
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
