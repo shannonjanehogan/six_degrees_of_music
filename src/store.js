bebb5a9a4788cec6ec027c5cd03fa77a7164dd78
mariaxtina <mchristinahess@gmail.com>
2018-12-15 08:55:47 -0800

    remove unused variable

--- src/store.js
+++ src/store.js
@@ -2,12 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/RootReducer';
 import {composeWithDevTools} from 'redux-devtools-extension'
-import pathReducer from './reducers/PathReducer';
 
 const configureStore = (initialState) => {
   const store = createStore(
     rootReducer,
-    // pathReducer,
     initialState,
     composeWithDevTools(applyMiddleware(thunk))
   )
